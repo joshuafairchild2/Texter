e28c39bd00aabc86a75914f77d0566b0edbb04b5
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-21 19:15:38 -0700

    complete 'APIs with MVC' lesson using Twilio api

---
+++ src/Texter/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Texter.Models;
+
+namespace Texter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetMessages()
+        {
+            List<Message> allMessages = Message.GetMessages();
+            return View(allMessages);
+        }
+
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Message newMessage)
+        {
+            newMessage.Send();
+            return RedirectToAction("Index");
+        }
+    }
+}
